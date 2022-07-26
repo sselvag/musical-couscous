982ffccfbad148f2288a27cc726a9c3bbcc2d87d
sselvag <swaathy.selva@gmail.com>
2022-07-26 03:11:53 -0400

    completed seeds

--- db/scripts.sql
+++ db/scripts.sql
@@ -1,11 +1,10 @@
--- everyone
 SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.dept_name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
 FROM employee 
 LEFT JOIN roles ON employee.role_id = roles.id 
 LEFT JOIN department ON roles.department_id = department.id 
 LEFT JOIN employee manager ON manager.id = employee.manager_id;
 
--- based on dept
+
 SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.dept_name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
 FROM employee 
 LEFT JOIN roles ON employee.role_id = roles.id 
--- index.js
+++ index.js
@@ -188,5 +188,118 @@ const initPrompt = () => {
     });
     };
 
+    const showAllRoles = () => {
+        let results = connection.query("SELECT * FROM roles;",
 
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+        })
+        initPrompt();
+    };
+
+    const addRole = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'roleTitle',
+                message: "What is the name of the role you would like to add?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid name";
+                }
+            },
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: "What is the salary for this role??"
+            },
+            {
+                type: 'input',
+                name: 'roleDept',
+                message: "What departmentID is this role under?"
+            },
+        ]).then(answers => {
+        let results = connection.query("INSERT INTO roles SET title = ?, salary = ?, department_id = ?",
+        [answers.roleTitle, answers.roleSalary, answers.roleDept],
+
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+        })
+        showAllRoles();
+        initPrompt();
+    });
+    };
+
+    const deleteEmployee = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'deleteEmployeeID',
+                message: "What is the id of the employee you would like to delete?"
+            },
+        ]).then(answers => {
+        let results = connection.query("DELETE FROM employee WHERE id = ?",
+        [answers.deleteEmployeeID],
+
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+        })
+        showAllEmployees();
+        initPrompt();
+    });
+    };
+
+    const updateEmployeeRole = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'updateEmployeeID',
+                message: "What is the id of the employee you would like to update the role for?"
+            },
+            {
+                type: 'input',
+                name: 'updateRoleID',
+                message: "What is the id of the role you would like to update this employee to?"
+            },
+        ]).then(answers => {
+        let results = connection.query("UPDATE employee SET role_id = ? WHERE id = ?",
+        [answers.updateRoleID, answers.updateEmployeeID],
+
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+        })
+        showAllEmployees();
+        initPrompt();
+    });
+    };
+
+    const updateEmployeeManager = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'updateManagerEmployeeID',
+                message: "What is the id of the employee you would like to update the manager for?"
+            },
+            {
+                type: 'input',
+                name: 'updateManagerID',
+                message: "What is the id of the manager you would like to update this employee to?"
+            },
+        ]).then(answers => {
+        let results = connection.query("UPDATE employee SET manager_id = ? WHERE id = ?",
+        [answers.updateManagerID, answers.updateManagerEmployeeID],
+
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+        })
+        showAllEmployees();
+        initPrompt();
+    });
 };
